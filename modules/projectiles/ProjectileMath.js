557d304e746289d57c7f334aad332f764cd8603b
Tektronica <ryalho@gmail.com>
2022-09-22 16:19:00 -0700

    Projectile given target angle and distance

--- modules/projectiles/ProjectileMath.js
+++ modules/projectiles/ProjectileMath.js
@@ -4,6 +4,7 @@ import { deg2rad, rad2deg } from "../base";
 
 const G = 9.81;  // m/s^2
 
+
 function find_position_at_time(h, theta, Vo, t) {
     const rad = deg2rad(theta);
     const Vy = Vo * Math.sin(rad);
@@ -24,6 +25,7 @@ function time_of_flight(h, theta, Vo) {
     return (Vy + Math.sqrt(Vy ** 2 + 2 * G * h)) / G;
 };
 
+
 function find_height_max(h, theta, Vo) {
     const rad = deg2rad(theta);
     const Vy = Vo * Math.sin(rad);
@@ -31,14 +33,44 @@ function find_height_max(h, theta, Vo) {
 };
 
 
-function get_launch_angle(V0, h, xfinal, yfinal) {
-    return rad2deg(Math.atan((V0 ** 2 + Math.sqrt(V0 ** 4 - G * (G * xfinal ** 2 + (2 * (yfinal - h) * (V0 ** 2))))) / (G * xfinal)));
+function get_launch_angle(type, params) {
+    var h = 0.0;  // launch height
+    var v0 = 0.0;  // launch (initial) velocity
+    var xfinal = 0.0;  // x coordinate to target
+    var yfinal = 0.0;  // y coordinate to target
+    var theta_final = 0.0;  // final angle to target
+
+    // https://stackoverflow.com/a/32138566/3382269
+    switch (type) {
+        case 'velocity':
+            // launch angle given the fixed velocity and coordinate distance to the target
+            ({ h, v0, xfinal, yfinal } = params);
+            return rad2deg(Math.atan((v0 ** 2 + Math.sqrt(v0 ** 4 - G * (G * xfinal ** 2 + (2 * (yfinal - h) * (v0 ** 2))))) / (G * xfinal)));
+
+        case 'angle':
+            // launch angle given the angle and coordinate distance to the target
+            ({ h, xfinal, yfinal, theta_final } = params);
+            const rad_final = deg2rad(theta_final);
+            return rad2deg(Math.atan((-xfinal * Math.tan(rad_final) - (2 * h) + (2 * yfinal)) / xfinal));
+
+        default:
+            throw new Error("case does not exist");
+    };
 };
+
+
+function get_launch_velocity(theta_initial, theta_final, xfinal) {
+    const rad_initial = deg2rad(theta_initial);
+    const rad_final = deg2rad(theta_final);
+    return (1 / Math.cos(rad_initial)) * Math.sqrt((G * xfinal) / (Math.tan(rad_initial) - Math.tan(rad_final)));
+};
+
 
 export {
     find_position_at_time,
     find_final_position,
     time_of_flight,
     find_height_max,
-    get_launch_angle
+    get_launch_angle,
+    get_launch_velocity
 };
--- pages/projectiles.js
+++ pages/projectiles.js
@@ -13,7 +13,8 @@ import {
     find_final_position,
     time_of_flight,
     find_height_max,
-    get_launch_angle
+    get_launch_angle,
+    get_launch_velocity
 } from "../modules/projectiles/ProjectileMath"
 
 export default function Projectiles() {
@@ -22,16 +23,40 @@ export default function Projectiles() {
     var launch_angle = 45;  // deg
     var launch_height = 2;  // m
 
-    const plotData01 = run_projectile_demo(launch_height, launch_angle, launch_velocity)
+    const plotData01 = run_projectile_demo(launch_height, launch_angle, launch_velocity);
 
     // second demo ---------------------------------------
-    launch_velocity = 10;  // m/s
     launch_height = 1;  // m
-    const target_distance = 8; // m
-    const target_height = 2;  // m
+    launch_velocity = 10;  // m/s
+    var target_distance = 8; // m
+    var target_height = 2;  // m
 
-    launch_angle = get_launch_angle(launch_velocity, launch_height, target_distance, target_height)
+    var params = {
+        h: launch_height, // m/s
+        v0: launch_velocity,  // meters
+        xfinal: target_distance, // meters
+        yfinal: target_height  // meters
+    }
+
+    launch_angle = get_launch_angle('velocity', params);
     const plotData02 = run_projectile_target_demo(launch_height, target_height, target_distance, launch_angle, launch_velocity);
+
+    // third demo ----------------------------------------
+    launch_height = 1;  // m/s
+    target_distance = 20;  // m
+    target_height = 3; // m
+    const target_angle = -45;  // m
+
+    params = {
+        h: launch_height, // meters
+        xfinal: target_distance,  // meters
+        yfinal: target_height, // meters
+        theta_final: target_angle  // theta
+    }
+
+    launch_angle = get_launch_angle('angle', params)
+    const Vo = get_launch_velocity(launch_angle, target_angle, target_distance)
+    const plotData03 = run_target_angle_demo(launch_height, target_height, target_distance, launch_angle, Vo)
 
     return (
         <>
@@ -46,7 +71,14 @@ export default function Projectiles() {
                 <h1 className="text-3xl font-bold underline">
                     Projectile Demo #2
                 </h1>
-                <TimePlot pointData={plotData02} title='Projectile Demo #1' />
+                <TimePlot pointData={plotData02} title='Projectile Demo #2' />
+            </ShadowBox>
+
+            <ShadowBox>
+                <h1 className="text-3xl font-bold underline">
+                    Projectile Demo #3
+                </h1>
+                <TimePlot pointData={plotData03} title='Projectile Demo #3' />
             </ShadowBox>
         </>
     )
@@ -122,6 +154,40 @@ function run_projectile_target_demo(h, hfinal, xfinal, theta, Vo) {
     return plotData
 };
 
+
+function run_target_angle_demo(h, hfinal, xfinal, theta, Vo) {
+    const rad = deg2rad(theta);
+    const Vx = Vo * Math.cos(rad);
+
+    // log the governing projectile equation
+    console.log(`Equation: y = ${h} + ${round(Math.tan(rad), 5)}x - ${round(G / (Vx ** 2 * 2.0), 5)}x^2`)
+
+    // compute final x position
+    // xfinal = find_final_position(h, theta, Vo)
+    console.log(`Distance = ${round(xfinal, 3)} m`)
+
+    // compute max height of projectile
+    const ymax = find_height_max(h, theta, Vo)
+    console.log(`Peak Height = ${round(ymax, 3)} m`)
+
+    // compute time elapsed
+    const tfinal = time_of_flight(h, theta, Vo)
+    console.log(`Time of Flight = ${round(tfinal, 3)} s`)
+
+    // plot
+    const xt = linspace(0, xfinal, 100);
+    const yt = xt.map((x) => (h + Math.tan(rad) * x - (G / (2.0 * Vx ** 2)) * (x ** 2)));
+
+    // convert to pointData rather than datasets xt=[...] and yt=[...]
+    // this could easily be a generator
+    let plotData = zeros([xt.length, 0]);
+
+    for (var idx in plotData) {
+        plotData[idx] = { x: xt[idx], y: yt[idx] };
+    };
+
+    return plotData
+}
 
 Projectiles.getLayout = function getLayout(page) {
     return (
